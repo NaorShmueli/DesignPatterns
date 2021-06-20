e110db94915b2028e6bb3a3360661a13ae3b855a
naor2 <naor2@DESKTOP-2DV3RHL>
2021-06-20 18:58:29 +0300

    singleton

---
+++ DesignPatterns/CreationalPatterns/Singleton/Implementations/SingletonObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton.Implementations
+{
+    /// <summary>
+    /// All data here shared
+    /// </summary>
+    sealed class SingletonObject
+    {
+        private static SingletonObject _instance = null;
+        private static readonly object _lockObj = new object();
+        private SingletonObject() { }
+
+        internal static SingletonObject GetInstance()
+        {
+            lock (_lockObj)
+            {
+                if (_instance == null)
+                {
+                    _instance = new SingletonObject();
+                }
+            }
+            
+            return _instance;
+        }
+    }
+}
---
+++ DesignPatterns/CreationalPatterns/Singleton/Program.cs
@@ -0,0 +1,21 @@
+﻿using Singleton.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var arrayTask = new List<Task>();
+            for(int i = 0; i < 1000; i++)
+            {
+                arrayTask.Add(Task.Factory.StartNew(() => SingletonObject.GetInstance()));
+            }
+
+            Task.WaitAll(arrayTask.ToArray());
+        }
+    }
+}
